5871c23ab3c1a8840f96db907e4612869b34b897
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-06 20:59:02 +0100

    decalation types of interface in ts

--- 04-InterseccionUnion/src/index.js
+++ 04-InterseccionUnion/src/index.js
@@ -1,6 +1,6 @@
 "use strict";
 //. Los tipos Union e Intersection ayudan a controlar situaciones en las que un tipo se compone de dos o más tipos posibles
-//Tipos de unión
+//Tipos de unión -- no se sabe el tipo de dato que tendra, Un tipo de unión describe un valor que puede ser uno de entre varios tipos
 let multiplesTipos;
 multiplesTipos = 20; //valido
 multiplesTipos = true; //valido
@@ -12,8 +12,22 @@ function add(x, y) {
     if (typeof x === 'string' && typeof y === 'string') {
         return x.concat(y);
     }
-    throw new Error('El parametro debe ser un numero o cadena');
+    //throw new Error('El parametro debe ser un numero o cadena')
 }
 console.log(add('one', 'two'));
 console.log(add(2, 3));
 console.log(add('one', 5));
+let nuevoGerente = {
+    empleadoID: 12345,
+    edad: 34,
+    accion: true
+};
+console.log(nuevoGerente);
+let miResultado;
+miResultado = "completo";
+//miResultado = "completar"; //error invalido
+miResultado = "incompleto";
+console.log(miResultado);
+let numero;
+numero = 5;
+console.log(numero);
