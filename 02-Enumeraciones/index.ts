27f681f1cc5ebce0a44ff70c05fd9aa31876fad3
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-06 19:36:21 +0100

    decalation of emuns in ts

--- 02-Enumeraciones/index.ts
+++ 02-Enumeraciones/index.ts
@@ -5,3 +5,16 @@ Puedes pensar en un enum como un listado especial de variables que tienen un con
 */
 
 
+//los enums comienzan de 0 a n, como un index, cada variable tiene un index predeternimado
+
+//si queremos que empiecen po rotro valor debemos especificar en la propiedad
+enum ContratoActual {
+   Permanente = 1,    //0
+   Temporal,     //1
+   Practica    //2
+}
+
+let estadoEmpleado: ContratoActual = ContratoActual.Temporal;
+
+//de esta manera se muestra el nombre asociado al valor... recordemos que estadoEmpelado es un numero
+console.log(ContratoActual[estadoEmpleado])
--- 02-Enumeraciones/src/index.js
+++ 02-Enumeraciones/src/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("hello word");
+//Un elemento enum es un nombre simbólico para un conjunto de valores
+/*
+Puedes pensar en un enum como un listado especial de variables que tienen un conjunto específico de valores posibles
+*/
+//los enums comienzan de 0 a n, como un index, cada variable tiene un index predeternimado
+//si queremos que empiecen po rotro valor debemos especificar en la propiedad
+var ContratoActual;
+(function (ContratoActual) {
+    ContratoActual[ContratoActual["Permanente"] = 1] = "Permanente";
+    ContratoActual[ContratoActual["Temporal"] = 2] = "Temporal";
+    ContratoActual[ContratoActual["Practica"] = 3] = "Practica"; //2
+})(ContratoActual || (ContratoActual = {}));
+let estadoEmpleado = ContratoActual.Temporal;
+//de esta manera se muestra el nombre asociado al valor... recordemos que estadoEmpelado es un numero
+console.log(ContratoActual[estadoEmpleado]);
