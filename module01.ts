20f54f57d4bf29c9dd7aca74a761a41805a8a68b
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-06 12:56:02 +0100

    first commit

---
+++ module01.ts
@@ -0,0 +1,2 @@
+//https://learn.microsoft.com/es-es/training/modules/typescript-get-started/6-typescript-project
+//https://www.youtube.com/watch?v=pFnYwVqANy8&list=PL5sOO-cTNRUVBoyh-7UIzSYTswoIGtdIo&index=11
