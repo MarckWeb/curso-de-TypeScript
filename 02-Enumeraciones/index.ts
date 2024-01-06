d4811edc878ac6bc186f7416625cdcbca8beda29
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-06 19:49:51 +0100

    decalation of emuns in ts

--- 02-Enumeraciones/index.ts
+++ 02-Enumeraciones/index.ts
@@ -18,3 +18,35 @@ let estadoEmpleado: ContratoActual = ContratoActual.Temporal;
 
 //de esta manera se muestra el nombre asociado al valor... recordemos que estadoEmpelado es un numero
 console.log(ContratoActual[estadoEmpleado])
+
+
+//Enums Numerico
+enum Dia {
+   Lunes,
+   Martes,
+   Miercoles,
+   Jueves,
+   Viernes
+}
+console.log(Dia.Martes); //1
+
+let dia: Dia = Dia.Jueves;
+console.log(Dia[dia]) //accedemos a su nombre jueves (no valor)
+
+
+//Enums Cadena
+enum Color {
+   Rojo = "Rojo",
+   Verde = "Verde",
+   Azul = "Azul"
+}
+console.log(Color.Verde)
+let color: Color = Color.Rojo
+console.log(color)
+
+//Enums Especificos
+enum Talla {
+   Pequeno = 1,
+   Mediano = 2,
+   Grande = 3
+}
--- 02-Enumeraciones/src/index.js
+++ 02-Enumeraciones/src/index.js
@@ -14,3 +14,32 @@ var ContratoActual;
 let estadoEmpleado = ContratoActual.Temporal;
 //de esta manera se muestra el nombre asociado al valor... recordemos que estadoEmpelado es un numero
 console.log(ContratoActual[estadoEmpleado]);
+//Enums Numerico
+var Dia;
+(function (Dia) {
+    Dia[Dia["Lunes"] = 0] = "Lunes";
+    Dia[Dia["Martes"] = 1] = "Martes";
+    Dia[Dia["Miercoles"] = 2] = "Miercoles";
+    Dia[Dia["Jueves"] = 3] = "Jueves";
+    Dia[Dia["Viernes"] = 4] = "Viernes";
+})(Dia || (Dia = {}));
+console.log(Dia.Martes); //1
+let dia = Dia.Jueves;
+console.log(Dia[dia]); //accedemos a su nombre jueves (no valor)
+//Enums Cadena
+var Color;
+(function (Color) {
+    Color["Rojo"] = "Rojo";
+    Color["Verde"] = "Verde";
+    Color["Azul"] = "Azul";
+})(Color || (Color = {}));
+console.log(Color.Verde);
+let color = Color.Rojo;
+console.log(color);
+//Enums Especificos
+var Talla;
+(function (Talla) {
+    Talla[Talla["Pequeno"] = 1] = "Pequeno";
+    Talla[Talla["Mediano"] = 2] = "Mediano";
+    Talla[Talla["Grande"] = 3] = "Grande";
+})(Talla || (Talla = {}));
