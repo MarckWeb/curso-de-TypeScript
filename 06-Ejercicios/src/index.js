1d61080dd433ef147989db660f06574d0bf2c757
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-07 10:25:59 +0100

    resolv exercises from the first part

---
+++ 06-Ejercicios/src/index.js
@@ -0,0 +1,3 @@
+"use strict";
+//https://learn.microsoft.com/es-es/training/modules/typescript-declare-variable-types/8-types-lab
+// https://learn.microsoft.com/es-es/training/modules/typescript-declare-variable-types/10-summary-resources
