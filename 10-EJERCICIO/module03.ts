0e4540566105bbccaaaf1b464fcb10a4f11aa27b
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-07 21:19:59 +0100

    solved exercise of the ts course with interfaces and strong typing

---
+++ 10-EJERCICIO/module03.ts
@@ -0,0 +1,40 @@
+/* Module 3: Implement interfaces in TypeScript
+   Lab Start  */
+
+/*  EXERCISE 1
+    TODO: Declare the Loan interface. */
+interface Loan {
+    principal: number,
+    interestRate: number
+}
+/*  TODO: Declare the ConventionalLoan interface. */
+interface ConventionalLoan extends Loan {
+    months: number
+}
+
+
+/*  TODO: Update the calculateInterestOnlyLoanPayment function. */
+
+function calculateInterestOnlyLoanPayment(loan: Loan) {
+    // Calculates the monthly payment of an interest only loan
+    let interest = loan.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let payment;
+    payment = loan.principal * interest;
+    return 'The interest only loan payment is ' + payment.toFixed(2);
+}
+
+/*  TODO: Update the calculateConventionalLoanPayment function. */
+
+function calculateConventionalLoanPayment(loanTerms: ConventionalLoan) {
+    // Calculates the monthly payment of a conventional loan
+    let interest: number = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let payment: number;
+    payment = loanTerms.principal * interest / (1 - (Math.pow(1 / (1 + interest), loanTerms.months)));
+    return 'The conventional loan payment is ' + payment.toFixed(2);
+}
+
+let interestOnlyPayment = calculateInterestOnlyLoanPayment({ principal: 30000, interestRate: 5 });
+let conventionalPayment = calculateConventionalLoanPayment({ principal: 30000, interestRate: 5, months: 180 });
+
+console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
+console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 
