bdd01d89931733bca53e86a3a7a4df684f1db605
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-08 13:44:27 +0100

    declare functions whit parameters rest and name propertyes in ts

---
+++ 11-Funciones/src/prueba.js
@@ -0,0 +1,27 @@
+"use strict";
+//FUNCIONES DECLARADAS CON TIPOS E INTERFACES
+//creamos dos varaibles usando calculator
+let addNumbers = (x, y) => x + y;
+let subtractNumbers = (x, y) => x - y;
+console.log(addNumbers(1, 2));
+console.log(subtractNumbers(1, 2));
+//También puede utilizar el tipo de función calculator para pasar valores de otra función
+let doCalculation = (operation) => {
+    if (operation === 'add') {
+        return addNumbers;
+    }
+    else {
+        return subtractNumbers;
+    }
+};
+console.log(doCalculation('add')(1, 2));
+console.log(doCalculation('subtract')(1, 2));
+//probando declarar funciones con interface
+// interface Calculator {
+//    (x: number, y: number): number;
+//sobre todo las intefaces en un funciones son muy inprotantes en clases.
+// }
+//al llamar  a la funcion no es necesario que los nombres de los parametros sean iguales
+// let addNumbers: Calculator = (x: number, y: number): number => x + y;
+// let addNumbers: Calculator = (number1: number, number2: number): number => number1 + number2;
+// let addNumbers: Calculator = (num1, num2) => num1 + num2;
