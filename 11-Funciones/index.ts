a1d2a75ea8c54db5e9d322b2a003bc78de72e1a8
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2024-01-08 15:17:06 +0100

    functions whit parameters rest and name propertyes in ts

--- 11-Funciones/index.ts
+++ 11-Funciones/index.ts
@@ -46,3 +46,37 @@ const total2 = (arrayInput: number[]): number => {
    }
    return total;
 }
+
+
+//------------------
+const aprendiendo = function (tecnologia: string, tecnologia2: string) {
+   console.log(`Aprendiendo ${tecnologia} y ${tecnologia2}`);
+}
+
+aprendiendo('TypeScript', 'Node.js')
+// arrow function
+
+const aprendiendo2 = (tecnologia: string, tecnologia2: string) => `Aprendiendo ${tecnologia} y ${tecnologia2}`;
+console.log(aprendiendo2('TypeScript', 'Node.js'));
+
+
+//----------
+
+interface Carrito {
+   Nombre: string;
+   precio: number;
+}
+const carrito: Carrito[] = [
+   { Nombre: "Monitor 30 pul", precio: 500 },
+   { Nombre: "Tablet", precio: 400 },
+   { Nombre: "Nevera", precio: 550 },
+   { Nombre: "Televisor", precio: 700 },
+   { Nombre: "Celular", precio: 900 },
+]
+
+const nuevoArray = carrito.map(p => `${p.Nombre} - Precio: ${p.precio}`)
+
+carrito.forEach(p => console.log(`${p.Nombre} - Precio: ${p.precio} `));
+
+
+console.log(nuevoArray);
